d7f2a5b6e9dc8d72d8594df16695bb39b03a4180
Marco Camilli <marco@emavap.be>
2025-03-24 17:40:34 +0100

    .

--- custom_components/huawei_ac_charger/sensor.py
+++ custom_components/huawei_ac_charger/sensor.py
@@ -1,28 +1,30 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .tcp_client import HuaweiTCPClient
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    client: HuaweiTCPClient = hass.data["huawei_ac_charger"][entry.entry_id]["client"]
     sensors = [
         ("Phase L1 Voltage", "V", 0x1000),
         ("Phase L2 Voltage", "V", 0x1002),
         ("Phase L3 Voltage", "V", 0x1004),
         ("Total Power", "kW", 0x100C)
     ]
-    client = hass.data["huawei_ac_charger"][entry.entry_id]["client"]
-    entities = [HuaweiSensor(client, name, unit, addr) for name, unit, addr in sensors]
-    async_add_entities(entities)
+    entities = [HuaweiSensor(client, name, unit, address) for name, unit, address in sensors]
+    async_add_entities(entities, update_before_add=True)
 
-class HuaweiSensor(CoordinatorEntity, SensorEntity):
+class HuaweiSensor(SensorEntity):
     def __init__(self, client, name, unit, address):
-        super().__init__(None)
         self._client = client
         self._attr_name = name
         self._attr_native_unit_of_measurement = unit
         self._address = address
 
     async def async_update(self):
-        result = await self._client.read_holding_registers(self._address, 2)
-        if result.isError():
-            self._attr_native_value = None
+        request_frame = b'\\x00\\x01\\x00\\x00\\x00\\x06\\x01\\x03' + self._address.to_bytes(2, 'big') + b'\\x00\\x02'
+        response = await self._client.send_request(request_frame)
+        if response and len(response) >= 9:
+            self._attr_native_value = int.from_bytes(response[9:13], 'big') / 10
         else:
-            self._attr_native_value = int.from_bytes(result.registers, "big") / 10
+            self._attr_native_value = None
