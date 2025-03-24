3a9c906df44683075d7fd6e95523719a9a25fe6c
Marco Camilli <marco@emavap.be>
2025-03-24 17:29:55 +0100

    .

--- custom_components/huawei_ac_charger/__init__.py
+++ custom_components/huawei_ac_charger/__init__.py
@@ -5,7 +5,7 @@ PLATFORMS = ["sensor", "number", "select", "binary_sensor"]
 
 async def async_setup_entry(hass, entry):
     hass.data.setdefault("huawei_ac_charger", {})
-    client = AsyncModbusTcpClient(entry.data["ip_address"], port=entry.data["port"])
+    client = AsyncModbusTcpClient(host=entry.data["ip_address"], port=entry.data["port"])
     await client.connect()
     hass.data["huawei_ac_charger"][entry.entry_id] = {"client": client}
     hass.async_create_task(
